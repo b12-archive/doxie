e079a00c41594ac021ed0b37d14def9b6ee556de
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-09 10:37:24 +0200

    Fail when trying to use programmatically

---
+++ index.js
@@ -0,0 +1,4 @@
+throw new Error(
+  'This module is only intended for command line usage. Look into If you ' +
+  'want to use doxie programmatically, look into <http://npm.im/doxie-core>.'
+);
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 var test = require('tape-catch');
+
+test('Programmatic usage fails', function(is) {
+  is.throws(
+    function() {require('./index');},
+    /look into .*doxie-core/i,
+    'with a helpful message'
+  );
+
+  is.end();
+});
