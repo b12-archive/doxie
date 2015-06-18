28bf927c4463834ff3144d41011418480b49e487
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-18 10:39:07 +0200

    Test usage help

--- test/bin/doxie.test.js
+++ test/bin/doxie.test.js
@@ -1,5 +1,50 @@
 const test = require('tape-catch');
+const {spawn} = require('nexpect');
+const {resolve} = require('path');
+
+const doxie = resolve(__dirname, '../../module/bin/doxie.js');
 
 test('The CLI program works', (is) => {
-  is.end();
+  is.plan(6);
+
+  spawn(
+    `${doxie}`, {stream: 'stderr'}
+  ).run((error, output, exit) => {
+    if (error) throw error;
+
+    is.equal(exit, 1,
+      '`doxie` fails…'
+    );
+
+    is.ok(
+      /^usage:/i.test(output),
+      'and prints usage'
+    );
+  });
+
+  spawn(
+    `${doxie} -h`
+  ).run((error, output, exit) => {if (error) throw error;
+    is.equal(exit, 0,
+      '`doxie -h` succeeds…'
+    );
+
+    is.ok(
+      /^usage/i.test(output),
+      'and prints usage'
+    );
+  });
+
+  spawn(
+    `${doxie} --help`
+  ).run((error, output, exit) => {if (error) throw error;
+    is.equal(exit, 0,
+      '`doxie --help` succeeds…'
+    );
+
+    is.ok(
+      /SYNOPSIS/.test(output),
+      'and prints manpage-like help'
+    );
+  });
 });
