0c8aaf6eed5a2b4ee5b2da6b1d3cd645b45f8b18
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-18 14:00:09 +0200

    Test graceful failure (WIP)

--- test/bin/doxie.test.js
+++ test/bin/doxie.test.js
@@ -1,8 +1,9 @@
 const tape = require('tape-catch');
 const {spawn} = require('nexpect');
 const {resolve} = require('path');
-const {test, plus, curry} = require('1-liners');
+const {test, plus, curry, property, map, shave} = require('1-liners');
 
+const kill = shave(1, process.kill);
 const title = curry(plus)('The CLI program:  ');
 const doxie = resolve(__dirname, '../../module/bin/doxie.js');
 
@@ -49,4 +50,54 @@ tape(title('Prints usage'), (is) => {
       '…and prints manpage-like help'
     );
   });
+});
+
+tape(title('Fails gracefully'), (is) => {
+  is.plan(4);
+
+  const pids = map(curry(property)('pid'), [
+    spawn(
+      `${doxie} --output`,
+      {stream: 'stderr'}
+    )
+      .sendEof()
+      .run((error, output, exit) => {if (error) throw error;
+        is.equal(exit, 1,
+          'piping empty input into `doxie --<plugin>` fails…'
+        );
+
+        is.ok(
+          test(output.join('\n'), /invalid json input/i),
+          '…and results in a helpful message'
+        );
+      })
+    ,
+
+    spawn(
+      `${doxie} --output`,
+      {stream: 'stderr'}
+    )
+      .sendline('not JSON')
+      .sendEof()
+      .run((error, output, exit) => {if (error) throw error;
+        is.equal(exit, 1,
+          'piping invalid JSON into `doxie --<plugin>` fails…'
+        );
+
+        is.ok(
+          test(output.join('\n'), /invalid json input/i),
+          '…and results in a helpful message'
+        );
+      })
+    ,
+  ]);
+
+  // Kill any dangling processes.
+  setTimeout(() => {
+    try {
+      pids.forEach(kill);
+    } catch (error) {
+      if (error.code !== 'ESRCH') throw error;
+    }
+  }, 500);
 });
