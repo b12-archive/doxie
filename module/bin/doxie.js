ee84fd5a567356ef7eddb9a1489376749afb67b2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-06-18 09:29:33 +0200

    Sort out output and exit codes

--- module/bin/doxie.js
+++ module/bin/doxie.js
@@ -2,30 +2,41 @@
 
 var args = process.argv.slice(2);
 
+const leave = (message) => {
+  process.stdout.write(`${message}\n`);
+  process.exit(0);
+};
+
+const flee = (message) => {
+  process.stderr.write(`${message}\n`);
+  process.exit(1);
+};
+
 // Usage info
-if (!args.length || args[0] === '-h') return console.log(
+if (!args.length) flee(
   require('./help/usage')
-); else if (args[0] === '--help') return console.log('\n' + [
-  require('./help/synopsis'),
-  require('./help/description'),
-  require('./help/examples'),
-  require('./help/popularPlugins'),
-].join('\n\n\n'));
+);
+
+if (args[0] === '-h') leave(
+  require('./help/usage')
+);
+
+if (args[0] === '--help') leave(
+  '\n' + [
+    require('./help/synopsis'),
+    require('./help/description'),
+    require('./help/examples'),
+    require('./help/popularPlugins'),
+  ].join('\n\n\n')
+);
 
 // Imports
 var doxie = require('doxie-core');
 var toJson = require('stream-to-json');
 var implode = require('1-liners/implode');
 
-var messagePrefix = require('chalk').cyan('[doxie]') + ' ';
-var tinyError = require('tiny-error')({
-  prefix: messagePrefix,
-});
-
-function fail (content) {
-  process.stderr.write(messagePrefix + content + '\n');
-  process.exit(1);
-};
+var prefix = require('chalk').cyan('[doxie]') + ' ';
+var tinyError = require('tiny-error')({prefix});
 
 // Argument parsing
 var pluginName = /^--(.+)$/;
@@ -53,8 +64,8 @@ var plugins = args.reduce(function(plugins, argument, index) {
 
 // The logic
 toJson(process.stdin, function (error, data) {
-  if (error) fail(
-    'Invalid JSON input: “' + error.message + '”.\n'
+  if (error) flee(
+    `${prefix}Invalid JSON input: “${error.message}”.\n`
   );
 
   doxie(
